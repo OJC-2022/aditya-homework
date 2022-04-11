6ba573460e6b11cfa4c45e6cb346af6a3fa6bbdc
Aditya Raj <64512818+18yearoldmonk@users.noreply.github.com>
2022-04-11 12:00:49 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -3,11 +3,7 @@ import java.util.*;
 
 public class Prime {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter no to check prime or not");
-		int a=sc.nextInt();
+	public void Prime1(int a) {
 		for(int i=2;i<a/2;i++) {
 			if(a%i==0) {
 				System.out.println("not prime");
@@ -17,6 +13,14 @@ public class Prime {
 				System.out.println("prime");
 				break;
 		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Prime ab=new Prime();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter no to check prime or not");
+		int a=sc.nextInt();
+		ab.Prime1(a);
 		
 
 	}
