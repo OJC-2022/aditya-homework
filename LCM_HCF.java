40daa229838999f2b1cd506946769525256eeee4
Aditya Raj <64512818+18yearoldmonk@users.noreply.github.com>
2022-04-11 11:58:46 +0530

    Update LCM_HCF.java

--- LCM_HCF.java
+++ LCM_HCF.java
@@ -3,14 +3,7 @@ import java.util.*;
 
 
 public class LCM_HCF {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enetr first no:");;
-		int a=sc.nextInt();
-		System.out.println("enter second no:");
-		int b=sc.nextInt();
+	public void LCM(int a,int b){
 		int x=a,y=b;
 		while(b!=0) {
 			if(a>b) {
@@ -23,6 +16,17 @@ public class LCM_HCF {
 		}
 		System.out.println("the HCF is: "+ a);
 		System.out.println("the lcm is: "+ (x*y)/a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LCM_HCF ab=new LCM_HCF();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter first no:");;
+		int a=sc.nextInt();
+		System.out.println("enter second no:");
+		int b=sc.nextInt();
+		ab.LCM(a, b);
 	}
 
 }
