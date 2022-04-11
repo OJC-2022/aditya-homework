937f4f24163bf1a56a30d14cf9f70395f1792c08
Aditya Raj <64512818+18yearoldmonk@users.noreply.github.com>
2022-04-11 11:45:21 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -2,18 +2,22 @@ package online_class;
 import java.util.*;
 
 public class Even {
+	public void check(int a) {
+		if(a%2==0){
+		    System.out.println("Even");
+		}
+		else{
+		    System.out.println("Odd");
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 Even ab=new Even();
 		 Scanner sc=new Scanner(System.in);
 		    System.out.println("enter the no");
 			int a=sc.nextInt();
-			if(a%2==0){
-			    System.out.println("Even");
-			}
-			else{
-			    System.out.println("Odd");
-			}
+			ab.check(a);
 	}
 
 }
