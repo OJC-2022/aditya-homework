a3bf19c70e8ddac74533d886be1eeb33f3f4f067
Aditya Raj <64512818+18yearoldmonk@users.noreply.github.com>
2022-04-11 12:06:53 +0530

    Update PrintEven.java

--- PrintEven.java
+++ PrintEven.java
@@ -2,16 +2,24 @@ package online_class;
 import java.util.*;
 public class PrintEven {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter term");
-		int n=sc.nextInt();
+	public void PrintEven(int n) {
 		while(n>1) {
 			System.out.println(n);
 			n-=2;
 		}
-
 	}
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		PrintEven ab =new PrintEven();
+		System.out.println("enter term");
+		int n=sc.nextInt();
+		if(n%2==0) {
+		ab.PrintEven(n);
+	}
+		else {
+			n=n-1;
+			ab.PrintEven(n);
+		}
+	}
 }
