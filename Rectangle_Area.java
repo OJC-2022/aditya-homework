22d47f9dcad06049fb12491259f5de90eaf7bbf2
Aditya Raj <64512818+18yearoldmonk@users.noreply.github.com>
2022-04-11 12:15:35 +0530

    Update Rectangle_Area.java

--- Rectangle_Area.java
+++ Rectangle_Area.java
@@ -1,15 +1,19 @@
 package online_class;
 import java.util.*;
 public class Rectangle_Area {
+	public void Rectangle_Area(int l,int b) {
+		System.out.println("The area is:" + l*b);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in); 
+		Scanner sc=new Scanner(System.in);
+		Rectangle_Area ab=new Rectangle_Area();
 			System.out.println("enter length:");
 			int l=sc.nextInt();
 			System.out.println("Enter breadth:");
 			int b=sc.nextInt();
-			System.out.println("The area is:" + l*b);
+			ab.Rectangle_Area(l,b);
 		
 
 	}
