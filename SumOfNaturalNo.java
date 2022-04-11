8da375c99db9200a16be1e0173d51bfd2a906237
Aditya Raj <64512818+18yearoldmonk@users.noreply.github.com>
2022-04-11 12:17:10 +0530

    Update SumOfNaturalNo.java

--- SumOfNaturalNo.java
+++ SumOfNaturalNo.java
@@ -3,13 +3,17 @@ import java.util.*;
 import java.util.Scanner;
 
 public class SumOfNaturalNo {
+	public void SumOfNaturalNo(int n) {
+		System.out.println((n*(n+1))/2);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
+		SumOfNaturalNo ab=new SumOfNaturalNo();
 		System.out.println("enter no upto");
 		int n=sc.nextInt();
-		System.out.println((n*(n+1))/2);
+		ab.SumOfNaturalNo(n);
 
 	}
 
