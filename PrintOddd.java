54aa50aa5872d667595e7c1990adccf95ace3b4a
Aditya Raj <64512818+18yearoldmonk@users.noreply.github.com>
2022-04-11 12:11:30 +0530

    Update PrintOddd.java

--- PrintOddd.java
+++ PrintOddd.java
@@ -3,15 +3,7 @@ import java.util.*;
 import java.util.Scanner;
 
 public class PrintOddd {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enetr first no:");;
-		int a=sc.nextInt();
-		System.out.println("enter second no:");
-		int b=sc.nextInt();
-		
+	public void PrintOddd(int a,int b){
 		while(a<b) {
 			if(a%2!=0) {
 			System.out.println(a);
@@ -22,6 +14,18 @@ public class PrintOddd {
 			}
 		
 		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		PrintOddd ab=new PrintOddd();
+		System.out.println("enetr first no:");;
+		int a=sc.nextInt();
+		System.out.println("enter second no:");
+		int b=sc.nextInt();
+		ab.PrintOddd(a,b);
+		
 
 	}
 
