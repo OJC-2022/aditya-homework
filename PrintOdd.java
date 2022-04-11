cbb2149e177afda267f4b7f77e2b48b99e4be65d
Aditya Raj <64512818+18yearoldmonk@users.noreply.github.com>
2022-04-11 12:09:00 +0530

    Update PrintOdd.java

--- PrintOdd.java
+++ PrintOdd.java
@@ -1,21 +1,25 @@
 package online_class;
 import java.util.*;
-
 public class PrintOdd {
 
+	public void PrintOdd(int n) {
+		while(n>1) {
+			System.out.println(n);
+			n-=2;
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter no of terms:");
-		int a=sc.nextInt();
-		int i=1;
-		int b=1;
-		while(i<=a) {
-			System.out.println(b);
-			b+=2;
-			i++;
-		}
-
+		PrintOdd ab =new PrintOdd();
+		System.out.println("enter term");
+		int n=sc.nextInt();
+		if(n%2!=0) {
+		ab.PrintOdd(n);
 	}
-
+		else {
+			n=n-1;
+			ab.PrintOdd(n);
+		}
+	}
 }
